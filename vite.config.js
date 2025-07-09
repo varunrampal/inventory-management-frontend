4a1591bb5b45b160f8bdf3d563e9663aca7fcb8b
alexamber2021@gmail.com <alexamber2021@gmail.com>
2025-07-09 15:42:47 -0700

    vite-config-changes

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,26 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/admin': 'https://inventory-management-server-vue1.onrender.com'
+//     },
+//     historyApiFallback: true
+//   },
+  
+// });
+
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/admin': 'https://inventory-management-server-vue1.onrender.com'
-    },
-    historyApiFallback: true
+    }
   },
-  
+  build: {
+    outDir: 'dist'
+  }
 });
+
